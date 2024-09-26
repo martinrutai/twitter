11d1718a84085ec2c864a865a431ad32831b948c
RutaiMartin <145286404+RutaiMartin@users.noreply.github.com>
2024-09-26 08:23:33 +0200

    zaciatky

--- twitter/routes/web.php
+++ twitter/routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
+});
+
+Route::get('/profile', function () {
+    return view('profile');
 });
