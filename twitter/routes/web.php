d35d496bb81e051ffb8b8980b6d5affb0e5fed64
RutaiMartin <145286404+RutaiMartin@users.noreply.github.com>
2024-09-26 08:41:47 +0200

    FUNGUJ

--- twitter/routes/web.php
+++ twitter/routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [DashboardController::class,'index']);
 
-Route::get('/profile', function () {
-    return view('profile');
-});
+Route::get('/profile', [UserController::class,'index']);
