75735fa77641a97597a314df7f2d8dd76b350d9b
RutaiMartin <145286404+RutaiMartin@users.noreply.github.com>
2024-09-26 08:33:50 +0200

    afjfjafkljklafd

--- twitter/routes/web.php
+++ twitter/routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('dashboard');
-});
+Route::get('/', [DashboardController::class,'index']);
 
 Route::get('/profile', function () {
     return view('profile');
